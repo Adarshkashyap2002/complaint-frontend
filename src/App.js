9355effa098b9d9c725f415465a97ea3b2e4dfc0
Adarsh Priya kashyap <2102004_cse@gita.edu.in>
2026-03-10 00:34:18 +0530

    fix complaints import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Complaints from "./pages/Complaints";
 
 import Login from "./components/Login";
 import ComplaintList from "./components/ComplaintList";
