e9a0cacb5d66f36407dc485c2e414acb926d6f88
Adarsh Priya kashyap <2102004_cse@gita.edu.in>
2026-03-11 16:00:20 +0530

    fix complaints component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-
-
-
-import Complaints from "./pages/Complaints";
-import Complaints from "./components/Complaints";
-import Complaints from "./Complaints";
-
-
 import Login from "./components/Login";
 import ComplaintList from "./components/ComplaintList";
 import AddComplaint from "./components/AddComplaint";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
 
         <Route path="/" element={<Login />} />
 
         <Route
           path="/complaints"
-  element={
-    <ProtectedRoute>
-      <Complaints />
-    </ProtectedRoute>
+          element={
+            <ProtectedRoute>
+              <ComplaintList />
+            </ProtectedRoute>
           }
         />
 
@@ -43,11 +31,8 @@ function App() {
         />
 
       </Routes>
-
     </BrowserRouter>
-
   );
-
 }
 
 export default App;
